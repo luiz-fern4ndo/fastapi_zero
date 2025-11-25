e1576036177f15adaa31dfe8f251c91cc3603262
luiz Fernando <luiz-fernando@outlook.com>
2025-11-25 11:29:28 -0300

    Adicionada a primeira migração com alembic. Criada tabela de usuários

---
+++ tests/test_db.py
@@ -0,0 +1,24 @@
+from dataclasses import asdict
+
+from sqlalchemy import select
+
+from fastapi_zero.models import User
+
+
+def test_create_user(session, mock_db_time):
+    with mock_db_time(model=User) as time:
+        new_user = User(
+            username='test', email='test@test.com', password='secret'
+        )
+        session.add(new_user)
+        session.commit()
+        # session.refresh(new_user)
+        # session.close()
+        user = session.scalar(select(User).where(User.username == 'test'))
+    assert asdict(user) == {
+        'id': 1,
+        'username': 'test',
+        'email': 'test@test.com',
+        'password': 'secret',
+        'created_at': time,
+    }
