3692f0882d16b2367242de9a1189e9e0c675f858
luiz Fernando <luiz-fernando@outlook.com>
2026-07-23 11:32:07 -0300

    AULA 03: Criado schema UserDB, UserPublic, UserSchema e criada rota para criação de usuário em lista

--- main.py
+++ main.py
@@ -2,11 +2,22 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
-from schema import Message
+from schema import Message, UserDB, UserPublic, UserSchema
 
 app = FastAPI()
+
+database = []
 
 
 @app.get('/', response_model=Message, status_code=HTTPStatus.OK)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+    database.append(user_with_id)
+
+    return user_with_id
